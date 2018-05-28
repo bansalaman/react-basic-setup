b8bfb2a8f4c33080e85af50c138ae60dc8dc142c
Aman Bansal <amanbansal@github.com>
2018-05-28 09:03:14 +0530

    react basic setup, run npm start after npm install and for  production run npm run build

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return <div>
+            <h1>My  React Header!</h1>
+        </div>
+    }
+}
+
+export default Header;
